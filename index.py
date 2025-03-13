825b9ec0c9b9431be946b9b9d8991239b371844a
Jeter.chen <jeter.chen@admimdeMacBook-Air.local>
2025-03-13 10:10:25 +0800

    feat:add index function

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import datetime
 
-print("hello world.")
-print("good")
+def index(a, b):
+    return a + b
+index(10, 2)
 
 with open("log.txt", "a") as f:
     f.write(f"Job ran at: {datetime.datetime.now()}\n")
